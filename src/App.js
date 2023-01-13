1e7114e7585e7438e32ddbd53eaf8d5735a3421c
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-01-13 22:42:35 +0600

    button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { PhotoCamera } from "@mui/icons-material";
+import { Button } from "@mui/material";
+
 function App() {
-  return <div>hello mui</div>;
+  return (
+    <div>
+      <Button variant="outlined">Text</Button>
+      <Button variant="contained" component="label">
+        <PhotoCamera />
+        <input hidden accept="image/*" multiple type="file" />
+      </Button>
+    </div>
+  );
 }
 
 export default App;
